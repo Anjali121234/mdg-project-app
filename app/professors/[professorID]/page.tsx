831a85789eaaa0a4c9283656a193702826f12bf7
Harsh3050-art <harshrishi2007@gmail.com>
2026-02-01 10:36:11 +0530

    Update page.tsx

--- app/professors/[professorID]/page.tsx
+++ app/professors/[professorID]/page.tsx
@@ -152,7 +152,9 @@ const [currentUser, setCurrentUser] = useState("");
 }
 
 return (
- <div className="min-h-screen bg-gradient-to-br from-[#0f172a] to-[#020617] text-white px-4 sm:px-6 md:px-10 py-12">
+ <div className="min-h-screen bg-gradient-to-br from-[#0f172a] to-[#020617] text-white px-1 sm:px-6 md:px-10 py-12">
+
+
 
     
     {/* Professor Header */}
